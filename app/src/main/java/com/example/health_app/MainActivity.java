7ae9d5f08278605d6b24149b067a99a72fba5fdc
Anushka952 <109644599+Anushka952@users.noreply.github.com>
2022-11-10 17:14:39 +0530

    Add files via upload

--- app/src/main/java/com/example/health_app/MainActivity.java
+++ app/src/main/java/com/example/health_app/MainActivity.java
@@ -1,55 +1,96 @@
-package com.example.health_app;
+package com.example.medi;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+    ListView listView;
+    String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Kommunicate.init(context, 248edb5f13266b9a5e9e6978878f144fb);
+        listView = (ListView) findViewById(R.id.listViewSong);
+        displaysongs();
     }
 
-    public void BMI_method(android.view.View View){
-        Intent bmi_intent = new Intent(this, BMI.class);
-        startActivity(bmi_intent);
+
+    public ArrayList<Integer> findsong(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Field fields[]=R.raw.class.getFields();
+        for(int count=0;count<fields.length;count++){
+            Log.i("Raw Asset: ",fields[count].getName());
+            int resourseID = 0;
+            try {
+                resourseID = fields[count].getInt(fields[count]);
+            } catch (IllegalAccessException e) {
+                break;
+            }
+            arrayList.add(resourseID);
+        }
+        return arrayList;
     }
 
-    public void steps_method(android.view.View View){
-        Intent steps_intent = new Intent(this, Target.class);
-        startActivity(steps_intent);
+    void displaysongs(){
+        final ArrayList<Integer> mysongs = findsong();
+        items = new String[mysongs.size()];
+        for(int i=0;i< mysongs.size();i++){
+            items[i] =getResources().getResourceName(mysongs.get(i)).replace(".mp3","").replace("com.example.medi:raw/","");
+        }
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+               /* if(mediaPlayer==null){
+                    mediaPlayer= MediaPlayer.create(getApplicationContext(),mysongs.get(i));
+                }
+                mediaPlayer.start();*/
+                startActivity(new Intent(getApplicationContext(),playerActivity.class).putExtra("songs",mysongs).putExtra("pos",i));
+            }
+        });
     }
 
-    public void disease_method(android.view.View View){
-        Intent disease_intent = new Intent(this, Target.class);
-        startActivity(disease_intent);
+    class customAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            @SuppressLint({"ViewHolder", "InflateParams"})
+            View myview  = getLayoutInflater().inflate(R.layout.list_item,null);
+            TextView  txtsong= myview.findViewById(R.id.txtsongname);
+            txtsong.setSelected(true);
+            txtsong.setText(items[i]);
+            return myview;
+        }
     }
-
-    public void meditation_method(android.view.View View){
-        Intent meditation_intent = new Intent(this, Target.class);
-        startActivity(meditation_intent);
-    }
-
-    public void calories_method(android.view.View View){
-        Intent calories_intent = new Intent(this, Target.class);
-        startActivity(calories_intent);
-    }
-
-    public void alarm_method(android.view.View View){
-        Intent alarm_intent = new Intent(this, Target.class);
-        startActivity(alarm_intent);
-    }
-
-    public void chatbot_method(android.view.View View){
-        Intent chat_intent = new Intent(this, Target.class);
-        startActivity(chat_intent);
-    }
-
 }
+
